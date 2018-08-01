0c7ee1dc70743da95d1f7a345216137eb196007c
BlueLake291 <ducmanhbk.201@gmail.com>
2018-08-01 17:00:08 +0700

    upload folder Array

---
+++ Data-Structure/Array/Union_Intersection_SortedTwoArray.cpp
@@ -0,0 +1,30 @@
+// Given two sorted arrays, find their union and intersection.
+
+#include <iostream>
+#include <vector>
+
+std::vector<int> getUnionOf(const std::vector<int>& firstVec, const std::vector<int>& secondVec)
+{
+
+
+}
+
+
+std::vector<int> getIntersectionOf(const std::vector<int>& firstVec, const std::vector<int>& secondVec)
+{
+
+}
+
+
+
+int main()
+{
+	std::vector<int> firstVec = { 1, 3, 4, 5, 7 };
+	std::vector<int> secondVec = { 2, 3, 5, 6 };
+
+
+
+
+	system("pause");
+	return 0;
+}
