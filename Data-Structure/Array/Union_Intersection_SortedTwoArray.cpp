e35940af0e7e546d92b3f9e0c44cb4955446852c
BlueLake291 <ducmanhbk.201@gmail.com>
2018-08-01 17:34:28 +0700

    update file

--- Data-Structure/Array/Union_Intersection_SortedTwoArray.cpp
+++ Data-Structure/Array/Union_Intersection_SortedTwoArray.cpp
@@ -2,17 +2,88 @@
 
 #include <iostream>
 #include <vector>
+#include <iterator>
+
 
 std::vector<int> getUnionOf(const std::vector<int>& firstVec, const std::vector<int>& secondVec)
 {
+	int i = 0;			// the index of first vector. 
+	int j = 0;			// the index of second vector.
+	int sizeFistVec = firstVec.size();
+	int sizeSecondVec = secondVec.size();
 
+	std::vector<int> thirdVec;
 
+	for (; i < sizeFistVec && j < sizeSecondVec;)
+	{
+		if (firstVec[i] < secondVec[j])
+		{
+			thirdVec.push_back(firstVec[i]);
+			++i;
+		}
+		else if (firstVec[i] > secondVec[j])
+		{
+			thirdVec.push_back(secondVec[j]);
+			++j;
+		}
+		else
+		{
+			thirdVec.push_back(firstVec[i]);
+
+			++i; 
+			++j;
+		}
+	}
+
+	for (; i < sizeFistVec; ++i)
+	{
+		thirdVec.push_back(firstVec[i]);
+	}
+
+	for (; j < sizeSecondVec; ++j)
+	{
+		thirdVec.push_back(secondVec[j]);
+	}
+
+	return thirdVec;
 }
 
 
 std::vector<int> getIntersectionOf(const std::vector<int>& firstVec, const std::vector<int>& secondVec)
 {
+	int i = 0;			// the index of first vector. 
+	int j = 0;			// the index of second vector.
+	int sizeFistVec = firstVec.size();
+	int sizeSecondVec = secondVec.size();
 
+	std::vector<int> thirdVec;
+
+	for (; i < sizeFistVec && j < sizeSecondVec;)
+	{
+		if (firstVec[i] < secondVec[j])
+		{		
+			++i;
+		}
+		else if (firstVec[i] > secondVec[j])
+		{			
+			++j;
+		}
+		else
+		{
+			thirdVec.push_back(firstVec[i]);
+
+			++i;
+			++j;
+		}
+	}
+
+	return thirdVec;
+}
+
+void printArray(const std::vector<int>& vec)
+{
+	std::copy(vec.begin(), vec.end(), std::ostream_iterator<int>(std::cout, "  "));
+	std::cout << "\n";
 }
 
 
@@ -22,7 +93,9 @@ int main()
 	std::vector<int> firstVec = { 1, 3, 4, 5, 7 };
 	std::vector<int> secondVec = { 2, 3, 5, 6 };
 
+	std::vector<int> unionVec = getIntersectionOf(firstVec, secondVec);
 
+	printArray(unionVec);
 
 
 	system("pause");
