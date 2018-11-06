beefbaca6941b3ec8ff0e2b04c3dc3ef747c7eca
Phan Duc Manh <manh.phanduc@vietis.vn>
2018-11-06 17:52:55 +0700

    update files

---
+++ Practice-Common-Library/C++-Boost-Library/process-xml/catalog-xml.cpp
@@ -0,0 +1,94 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+#include <boost/property_tree/xml_parser.hpp>
+#include <boost\property_tree\ptree.hpp>
+#include <boost\foreach.hpp>
+
+
+struct CPicture
+{
+	std::string title; 
+	std::string nameArtist; 
+	std::string country;
+	std::string company;
+	float				price; 
+	int					year;
+};
+
+
+void readCatalogXml(std::istream& is, std::vector<CPicture>& vecPicture)
+{
+	using boost::property_tree::ptree;
+	ptree pt;
+	read_xml(is, pt);
+
+	BOOST_FOREACH(ptree::value_type const & v, pt.get_child("CATALOG"))
+	{
+		if (v.first == "CD")
+		{
+			CPicture picture;
+			picture.title = v.second.get<std::string>("TITLE");
+			picture.nameArtist = v.second.get<std::string>("ARTIST");
+			picture.country = v.second.get<std::string>("COUNTRY");
+			picture.company = v.second.get<std::string>("COMPANY");
+			picture.price = v.second.get<float>("PRICE");
+			picture.year = v.second.get<int>("YEAR");
+
+			vecPicture.push_back(std::move(picture));
+		}
+	}
+}
+
+void writeCatalogXml(std::ostream& os, const std::vector<CPicture>& vecPic)
+{
+	using boost::property_tree::ptree;
+	ptree pt;
+
+	BOOST_FOREACH(const CPicture& pic, vecPic)
+	{
+		ptree& node = pt.add("CATALOG.CD", "\n");
+		node.put("TITLE", pic.title + "\n");
+		node.put("ARTIST", pic.nameArtist + "\n");
+		node.put("COUNTRY", pic.country + "\n");
+		node.put("COMPANY", pic.company + "\n");
+		node.put("PRICE", std::to_string(pic.price) + "\n");
+		node.put("YEAR", std::to_string(pic.year) + "\n");
+	}
+
+	write_xml(os, pt);
+}
+
+void printContent(const std::vector<CPicture>& vecPic)
+{
+	for (auto pic : vecPic)
+	{
+		std::cout << pic.title << "\n";
+		std::cout << pic.nameArtist << "\n";
+		std::cout << pic.country << "\n";
+		std::cout << pic.company << "\n";
+		std::cout << pic.price << "\n";
+		std::cout << pic.year << "\n";
+		std::cout << "\n";
+	}	
+}
+
+
+int main()
+{
+	std::string fileIn = "./cd-catalog.xml";
+	std::vector<CPicture> vecPicture;
+
+	readCatalogXml(std::ifstream(fileIn), vecPicture);
+
+	//printContent(vecPicture);
+
+	std::string fileOut = "./cd-catalog-output.xml";
+
+	writeCatalogXml(std::ofstream(fileOut), vecPicture);
+
+	system("pause");
+	return 0;
+}
--- Practice-Common-Library/C++-Boost-Library/process-xml/read-write-xml.cpp
+++ Practice-Common-Library/C++-Boost-Library/process-xml/read-write-xml.cpp
@@ -1,22 +1,78 @@
 #include <boost\property_tree\ptree.hpp>
 #include <boost\property_tree\xml_parser.hpp>
 #include <boost\foreach.hpp>
+#include <boost\date_time\gregorian\gregorian.hpp>
 
 #include <vector>
 #include <string>
 #include <iostream>
+#include <locale>
 
+typedef boost::gregorian::date Date;
 
 struct Flight
 {
 	std::string carrier;
 	unsigned int number; 
-	std::string date; 
+	Date date; 
 	bool cancelled;
 };
 
 typedef std::vector<Flight> Sked;
 
+// convert string to Date type in Boost.
+class DateTranslator
+{
+private:
+	typedef boost::date_time::date_facet<Date, char> tOFacet;
+	typedef boost::date_time::date_input_facet<Date, char> tIFacet;
+	std::locale locale_;
+
+	static std::locale isoDateLocale()
+	{
+		std::locale loc; 
+		loc = std::locale(loc, new tIFacet("%Y-%m-%d"));
+		loc = std::locale(loc, new tOFacet("%Y-%m-%d"));
+		
+		return loc;
+	}
+
+public:
+	typedef std::string internal_type;
+	typedef Date external_type;
+
+	DateTranslator() : locale_(isoDateLocale()) {}
+
+	boost::optional<external_type> get_value(internal_type const& v)
+	{
+		std::istringstream stream(v);
+		stream.imbue(locale_);
+		external_type vAns;
+		if (stream >> vAns) {
+			return vAns;
+		}
+		else {
+			return boost::none;
+		}
+	}
+
+	boost::optional<internal_type> put_value(external_type const& v)
+	{
+		std::ostringstream ans;
+		ans.imbue(locale_);
+		ans << v;
+		return ans.str();
+	}
+};
+
+namespace boost {
+	namespace property_tree {
+
+		template<> struct translator_between<std::string, Date> {
+			typedef DateTranslator type;
+		};
+	}
+}
 
 Sked read(std::istream& is)
 {
@@ -32,7 +88,7 @@ Sked read(std::istream& is)
 			Flight f; 
 			f.carrier = v.second.get<std::string>("carrier");
 			f.number = v.second.get<unsigned int>("number");
-			f.date = v.second.get<std::string>("date");
+			f.date = v.second.get<Date>("date");
 			f.cancelled = v.second.get("<xmlattr>.cancelled", false);
 			ans.push_back(f);
 		}
