1534d56bb2da3c88849e39de3d4801d99fe1d986
Phan Duc Manh <manh.phanduc@vietis.vn>
2018-11-23 14:11:08 +0700

    update files

--- Algorithms/Dynamic Programming/LIS_tabulation.cpp
+++ Algorithms/Dynamic Programming/LIS_tabulation.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <vector>
-
+#include <algorithm>
 
 void makeTabulation(const std::vector<int>& arr, std::vector<int>& L, std::vector<int>& tracker)
 {
@@ -12,9 +12,10 @@ void makeTabulation(const std::vector<int>& arr, std::vector<int>& L, std::vecto
 
         for (int j = 0; j < i; ++j)
         {
-            if (arr[j] < arr[i] && lmax < L[j])
+            if (arr[j] < arr[j + 1] && lmax < L[j])
             {
-                
+                lmax = L[j];
+                jmax = j;
             }
         }
 
@@ -24,10 +25,21 @@ void makeTabulation(const std::vector<int>& arr, std::vector<int>& L, std::vecto
 }
 
 
+void printResult(const std::vector<int>& arr, const std::vector<int>& L, const std::vector<int>& tracker)
+{
+    std::vector<int>::iterator it = std::max(arr.begin(), arr.end());
+    int indexMaxElem = std::distance(arr.begin(), it);
+
+    std::cout << "The longest increase sequence of the above array is: " << "\n";
+
+
+}
+
+
 int main() 
 {
-    std::vector<int> arr = {5, 2, 3, 4, 9, 5, 6, 7, 8};
-    std::vector<int> L(arr.size(), 1);
+    std::vector<int> arr = {5, 2, 3, 4, 9, 5, 6, 7, 8, 1};
+    std::vector<int> L(arr.size(), 0);
     std::vector<int> tracker(arr.size(), -1);
 
 
