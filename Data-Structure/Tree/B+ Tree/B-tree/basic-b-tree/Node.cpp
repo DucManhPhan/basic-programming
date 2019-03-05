54e570fd72e5afbf1b7006a8825372eea8b6d33a
manh.phanduc@vietis.com <ducmanhphan93@gmail.com>
2019-03-05 17:47:52 +0700

    update files

--- Data-Structure/Tree/B+ Tree/B-tree/basic-b-tree/Node.cpp
+++ Data-Structure/Tree/B+ Tree/B-tree/basic-b-tree/Node.cpp
@@ -1,3 +1,23 @@
 #include "Node.h"
 
 
+CNode::CNode(int degreeKeys, bool leaf) : m_nMinDegreeKeys(degreeKeys)
+                                        , m_bLeaf(leaf)
+	                                      , m_nCurrentNumberKeys(0)
+	                                      , m_vtKeys(m_nCurrentNumberKeys)
+	                                      , m_vtChildNodes(0)
+{
+	// nothing to do
+}
+
+
+CNode* CNode::search(int key)
+{
+
+}
+
+
+void CNode::traverse()
+{
+
+}
--- Data-Structure/Tree/B+ Tree/B-tree/basic-b-tree/Node.h
+++ Data-Structure/Tree/B+ Tree/B-tree/basic-b-tree/Node.h
@@ -5,13 +5,19 @@
 
 class CNode {
 public:
-	CNode();
+	CNode(int degreeKeys, bool leaf);
 
+	void traverse();
 
+	CNode* search(int key);
+
+	friend class CBtree;
 
 private:
-	std::vector<int>			m_vtKeys;
-	std::vector<CNode*>		m_vtNodes;
+	std::vector<int>	    m_vtKeys;
+	std::vector<CNode*>		m_vtChildNodes;
 
-
+	int                     m_nCurrentNumberKeys;
+	int                     m_nMinDegreeKeys;
+	bool                    m_bLeaf;	
 };
