98199f1c7a524768483cef18723c70ffa59d1692
manh.phanduc@vietis.com <ducmanhphan93@gmail.com>
2019-03-05 18:16:09 +0700

    update files

--- Data-Structure/Tree/B+ Tree/B-tree/basic-b-tree/Btree.h
+++ Data-Structure/Tree/B+ Tree/B-tree/basic-b-tree/Btree.h
@@ -1,15 +1,21 @@
 #pragma once
 
 #include "pch.h"
+#include "Node.h"
 
 
 class CBtree {
 public:
+	CBtree(int degreeKeys);
+
+	void traverse();
+
+	CNode* search(int key);
 
 
 
 
 private:
-
-
+	CNode*          m_root;
+	int             m_nMinimumDegreeKeys;
 };
--- Data-Structure/Tree/B+ Tree/B-tree/basic-b-tree/Node.cpp
+++ Data-Structure/Tree/B+ Tree/B-tree/basic-b-tree/Node.cpp
@@ -4,8 +4,8 @@
 CNode::CNode(int degreeKeys, bool leaf) : m_nMinDegreeKeys(degreeKeys)
                                         , m_bLeaf(leaf)
 	                                      , m_nCurrentNumberKeys(0)
-	                                      , m_vtKeys(m_nCurrentNumberKeys)
-	                                      , m_vtChildNodes(0)
+	                                      , m_vtKeys(2 * m_nMinDegreeKeys - 1)
+	                                      , m_vtChildNodes(2 * m_nMinDegreeKeys)
 {
 	// nothing to do
 }
@@ -13,11 +13,43 @@ CNode::CNode(int degreeKeys, bool leaf) : m_nMinDegreeKeys(degreeKeys)
 
 CNode* CNode::search(int key)
 {
+	int i = 0;
 
+	while (i < m_nCurrentNumberKeys && key > m_vtKeys[i])
+	{
+		++i;
+	}
+
+	if (m_vtKeys[i] == key)
+	{
+		return this;
+	}
+
+	if (m_bLeaf)
+	{
+		return nullptr;
+	}
+
+	return m_vtChildNodes[i]->search(key);
 }
 
 
 void CNode::traverse()
 {
+	int i = 0;
 
+	for (i = 0; i < m_nCurrentNumberKeys; ++i)
+	{
+		if (!m_bLeaf)
+		{
+			m_vtChildNodes[i]->traverse();
+		}
+
+		std::cout << " " << m_vtKeys[i];
+	}
+
+	if (!m_bLeaf)
+	{
+		m_vtChildNodes[i]->traverse();
+	}
 }
