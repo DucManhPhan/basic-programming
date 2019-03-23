d1eba1329f3b41c817ced73da99e780fce0714c8
DucManhPhan <ducmanhphan93@gmail.com>
2019-03-23 08:34:04 +0700

    update files

---
+++ Network/python/src/multithreaded-server/multithreaded_server.py
@@ -0,0 +1,57 @@
+import socket
+
+from _thread import *
+import threading
+
+
+print_lock = threading.Lock()
+
+def threaded(c):
+    while True:
+        # data received from client
+        data = c.recv(1024);
+        if not data:
+            print("Bye");
+
+            # lock released on exit
+            print_lock.release()
+            break;
+        
+        #reverse the given string from client
+        data = data[::-1]
+
+        # send back reversed string to client
+        c.send(data);
+
+    #connection closed
+    c.closed()
+
+def Main():
+    host = ""
+
+    # reverse a port on our computer
+    port = 12345
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind((host, port))
+    print("socket binded to post", port)
+
+    s.listen(5);
+    print("Socket is listening.")
+
+    # Wait until client wants to exit
+    while True:
+
+        # establish connection with client
+        conn, addr = s.accept()
+
+        # lock acquired by client
+        print_lock.acquire()
+        print('Connected to: ', addr[0], ':', addr[1])
+
+        # Start a new thread and return its identifier
+        start_new_thread(threaded, (conn,))
+
+    s.close()
+
+if __name__ == '__main__':
+    Main()
