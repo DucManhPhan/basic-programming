b9a27c2b25da3641689f4de39cc60d48710966bd
ManhPD5 <ducmanhphan93@gmail.com>
2019-04-03 16:39:35 +0700

    update files

---
+++ Data-Structure/Find Union/weight-quick-union-path-compression.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+#include <fstream>
+
+
+using namespace std;
+
+const int MN = 2001;// capacity of array
+int id[MN];			// trees
+int n;				// number of elements
+int m;				// number of pairs
+int sz[MN];			// keep track the size of each tree
+
+
+void init()
+{
+	for (int i = 1; i <= n; ++i)
+	{
+		id[i] = i;
+		sz[i] = 1;
+	}
+}
+
+
+int root(int i)
+{
+	while (i != id[i])
+	{
+		id[i] = id[id[i]];	// First time, assign this tree to the grandparent of the tree's root node
+		i = id[i];			// Finally, assign parent of parent ... of the tree to the root node.
+	}
+
+	return i;
+}
+
+
+bool connected(int p, int q)
+{
+	return root(p) == root(q);
+}
+
+
+int quickUnion(int p, int q)
+{
+	int pid = root(p);
+	int qid = root(q);
+
+	if (pid == qid)
+	{
+		return 0;
+	}
+
+	if (sz[pid] > sz[qid])
+	{
+		id[qid] = pid;
+		sz[pid] += sz[qid];
+	}
+	else
+	{
+		id[pid] = qid;
+		sz[qid] += pid;
+	}
+
+	return 1;
+}
+
+
+
+int main()
+{
+
+
+
+	system("pause");
+	return 0;
+}
