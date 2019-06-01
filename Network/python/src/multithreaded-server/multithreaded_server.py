9555f840a4f5bd61574b30546f726ebf4ab6b024
DucManhPhan <ducmanhphan93@gmail.com>
2019-06-01 08:33:01 +0700

    update Multithreaded server

--- Network/python/src/multithreaded-server/multithreaded_server.py
+++ Network/python/src/multithreaded-server/multithreaded_server.py
@@ -9,19 +9,19 @@ print_lock = threading.Lock()
 def threaded(c):
     while True:
         # data received from client
-        data = c.recv(1024);
+        data = c.recv(1024)
         if not data:
-            print("Bye");
+            print("Bye")
 
             # lock released on exit
             print_lock.release()
-            break;
+            break
         
         #reverse the given string from client
         data = data[::-1]
 
         # send back reversed string to client
-        c.send(data);
+        c.send(data)
 
     #connection closed
     c.closed()
@@ -35,7 +35,7 @@ def Main():
     s.bind((host, port))
     print("socket binded to post", port)
 
-    s.listen(5);
+    s.listen(5)
     print("Socket is listening.")
 
     # Wait until client wants to exit
