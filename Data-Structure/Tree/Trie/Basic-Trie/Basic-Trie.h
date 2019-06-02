c0ee4b59bc698f19997a55a802b29e30e81bc086
DucManhPhan <ducmanhphan93@gmail.com>
2019-06-03 00:26:30 +0700

    update Basic Trie

--- Data-Structure/Tree/Trie/Basic-Trie/Basic-Trie.h
+++ Data-Structure/Tree/Trie/Basic-Trie/Basic-Trie.h
@@ -1,10 +1,12 @@
 #pragma once
-#include <iostream>
-#include <algorithm>
-#include <string>
-#include <vector>
+#include "TrieNode.h"
 
-#define ALPHABET_SIZE	 26
+
+enum Deletion_State {
+	SUCCESS = 0, 
+	ERROR, 
+	NO_EXIST,
+};
 
 
 class Trie
@@ -14,59 +16,17 @@ public:
 
 	void insert(const std::string& str);
 
-	bool del(const std::string& str);
+	int remove(const std::string& str);
 
 	bool search(const std::string& str);
 
 	bool startsWith(const std::string& prefix);
 
+private:
+	TrieNode* remove(TrieNode* pRoot, const std::string& str, int level = 0);
+
+	bool isEmpty(TrieNode* pRoot);
+
 private: 
-
-	class TrieNode
-	{
-	public: 
-		TrieNode() : m_vecChildren(ALPHABET_SIZE, nullptr), m_numLeafNodes(0)
-		{
-			// nothing to do.
-		}
-
-		TrieNode(char c) : m_vecChildren(ALPHABET_SIZE, nullptr)
-										 , m_numLeafNodes(0)
-										 , m_character(c)
-		{
-			// nothing to do.
-		}
-
-		std::vector<TrieNode*>& getChildren()
-		{
-			return m_vecChildren;
-		}
-
-		int getNumLeafNodes()
-		{
-			return m_numLeafNodes;
-		}
-
-		void setNumLeafNodes(int numLeafNodes)
-		{
-			m_numLeafNodes = numLeafNodes;
-		}
-
-		char getCharacter()
-		{
-			return m_character;
-		}
-
-		void setCharacter(char c)
-		{
-			m_character = c;
-		}
-
-	private: 
-		std::vector<TrieNode*>	m_vecChildren; 
-		int						m_numLeafNodes;
-		char					m_character;
-	};
-
 	TrieNode*					m_pRoot;
 };
---
+++ Data-Structure/Tree/Trie/Basic-Trie/TrieNode.h
@@ -0,0 +1,54 @@
+#pragma once
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+
+#define ALPHABET_SIZE	 26
+
+
+class TrieNode
+{
+public:
+	TrieNode() : m_vecChildren(ALPHABET_SIZE, nullptr), m_numLeafNodes(0)
+	{
+		// nothing to do.
+	}
+
+	TrieNode(char c) : m_vecChildren(ALPHABET_SIZE, nullptr)
+		, m_numLeafNodes(0)
+		, m_character(c)
+	{
+		// nothing to do.
+	}
+
+	std::vector<TrieNode*>& getChildren()
+	{
+		return m_vecChildren;
+	}
+
+	int getNumLeafNodes()
+	{
+		return m_numLeafNodes;
+	}
+
+	void setNumLeafNodes(int numLeafNodes)
+	{
+		m_numLeafNodes = numLeafNodes;
+	}
+
+	char getCharacter()
+	{
+		return m_character;
+	}
+
+	void setCharacter(char c)
+	{
+		m_character = c;
+	}
+
+private:
+	std::vector<TrieNode*>	m_vecChildren;
+	int						m_numLeafNodes;
+	char					m_character;
+};
