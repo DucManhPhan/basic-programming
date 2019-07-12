c653813fdbe6a1d9269331ad1580fbb220acec1c
manh.phanduc@vietis.com <>
2019-07-12 08:27:48 +0700

    push min-element-sorted-array

---
+++ Algorithms/Search algorithms/binary search/min-element-sorted-array.py
@@ -0,0 +1,5 @@
+# Suppose a sorted array A is rotated at some pivot unknown to you beforehand.
+# (i.e., 0 1 2 4 5 6 7 might become 4 5 6 7 0 1 2).
+# Find the minimum element.
+# The array will not contain duplicates.
+
