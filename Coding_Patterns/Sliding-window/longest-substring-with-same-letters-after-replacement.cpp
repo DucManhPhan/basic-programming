6e74062412d590a282f77d95fc1c754cb9958df4
ManhPD5 <ducmanhphan93@gmail.com>
2019-08-12 07:57:12 +0700

    update files

---
+++ Coding_Patterns/Sliding-window/longest-substring-with-same-letters-after-replacement.cpp
@@ -0,0 +1,34 @@
+// Given a string with lowercase letter only, if you are allowed to replace no more than k letters with any letters,
+// find the length of the longest substring having the same letters after replacement.
+
+// Input: String="aabccbb", k=2
+// Output: 5
+// Explanation : Replace the two 'c' with 'b' to have a longest repeating substring "bbbbb".
+
+// Input: String = "abbcb", k = 1
+// Output : 4
+// Explanation : Replace the 'c' with 'b' to have a longest repeating substring "bbbb".
+
+// Input: String = "abccde", k = 1
+// Output : 3
+// Explanation : Replace the 'b' or 'd' with 'c' to have the longest repeating substring "ccc".
+
+#include <iostream>
+#include <algorithm>
+#include <string>
+
+
+int findLengthOfLongestSubstring(const std::string& str)
+{
+
+}
+
+
+int main()
+{
+
+
+
+	system("pause");
+	return 0;
+}
--- Coding_Patterns/Sliding-window/no-repeat-substring.cpp
+++ Coding_Patterns/Sliding-window/no-repeat-substring.cpp
@@ -29,6 +29,7 @@ bool hasContains(const std::unordered_map<char, int>& distinctCharacters, char c
 }
 
 
+// Wrong when do not calculate the case: "aabcadb".
 int findMaxLength(const std::string& str)
 {
 	int size = str.size();
@@ -56,12 +57,40 @@ int findMaxLength(const std::string& str)
 }
 
 
+int findMaxLength_Other(const std::string& str)
+{
+	int windowStart = 0, maxLength = 0;
+
+	// save the last index of each character
+	std::unordered_map<char, int> charIndexMap;
+
+	for (int windowEnd = 0; windowEnd < str.length(); windowEnd++)
+	{
+		char rightChar = str[windowEnd];
+		// if the map already contains the 'rightChar', shrink the window from the beginning so that
+		// we have only one occurrence of 'rightChar'
+		if (charIndexMap.find(rightChar) != charIndexMap.end()) {
+			// this is tricky; in the current window, we will not have any 'rightChar' after its
+			// previous index and if 'windowStart' is already ahead of the last index of 'rightChar',
+			// we'll keep 'windowStart'
+			windowStart = std::max(windowStart, charIndexMap[rightChar] + 1);
+		}
+
+		charIndexMap[str[windowEnd]] = windowEnd;  // insert the 'rightChar' into the map
+		maxLength = std::max(maxLength, windowEnd - windowStart + 1);  // remember the maximum length so far
+	}
+
+	return maxLength;
+}
+
 
 int main()
 {
-	std::string str = "aaaaabbbb";  // "abcdef"	//  "aabccbb"	// "abccde"
+	std::string str = "aabcadb";  // "abcdef"	//  "aabccbb"	// "abccde"
 	
-	std::cout << findMaxLength(str);
+	std::cout << "Result of educative.io: " << findMaxLength_Other(str) << "\n";
+
+	std::cout << "Result: " << findMaxLength(str) << "\n";
 
 	system("pause");
 	return 0;
