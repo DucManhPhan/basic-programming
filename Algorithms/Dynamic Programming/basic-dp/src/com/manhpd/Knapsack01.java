f0ddfd8c1070726462bb8ba55f99314769cfbdd7
DucManhPhan <ducmanhphan93@gmail.com>
2020-04-25 20:58:49 +0700

    update files

--- Algorithms/Dynamic Programming/basic-dp/src/com/manhpd/Knapsack01.java
+++ Algorithms/Dynamic Programming/basic-dp/src/com/manhpd/Knapsack01.java
@@ -25,16 +25,10 @@ public class Knapsack01 {
     private static List<List<Integer>> realResults = new ArrayList<>();
 
     public static void main(String[] args) {
-//        String s = "7:05:45PM";
-//        String s = "12:05:45AM";
-        String s = "06:40:03AM";
-        String result24format = timeConversion(s);
-        System.out.println(result24format);
+        int[] weights = {2, 3, 1, 4};
+        int[] profits = {4, 5, 3, 7};
+        int capacity = 5;
 
-//        int[] weights = {2, 3, 1, 4};
-//        int[] profits = {4, 5, 3, 7};
-//        int capacity = 5;
-//
 //        long startMs = System.currentTimeMillis();
 //        bruteForce(weights, profits, capacity);
 //        long duration = System.currentTimeMillis() - startMs;
@@ -91,32 +85,6 @@ public class Knapsack01 {
         }
     }
 
-    private static String timeConversion(String s) {
-        String pmOrAm = s.substring(s.length() - 2, s.length());
-        String[] hhmmss = s.split(":");
-        String ss = hhmmss[2].substring(0, 2);
 
-        StringBuilder result = new StringBuilder();
-
-        if (pmOrAm.equals("AM"))  {
-            if (hhmmss[0].equals("12")) {
-                result.append("00");
-            } else {
-                result.append(hhmmss[0]);
-            }
-        } else if (pmOrAm.equals("PM")) {
-            if (hhmmss[0].equals("12")) {
-                result.append("12");
-            } else {
-                int hh = Integer.parseInt(hhmmss[0]) + 12;
-                result.append(String.valueOf(hh));
-            }
-        }
-
-        result.append(":" + hhmmss[1]);
-        result.append(":" + ss);
-
-        return result.toString();
-    }
 
 }
