6d895c81d17f0c1355bcce99961edb9f91fa5700
unknown <ducmanhphan93@gmail.com>
2020-07-24 17:31:09 +0700

    update files

---
+++ Coding_Patterns/Two-pointers/basic-two-pointers/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+Y
+)src/com/manhpd/TripletSumCloseTarget.java,b\c\bcb753df2a151c5e2ee428bd46b2afc1bf4632a9
+S
+#src/com/manhpd/LongPressedName.java,0\0\0024703e46ea30db252906de908d15935edd5494
--- Coding_Patterns/Two-pointers/basic-two-pointers/src/com/manhpd/LongPressedName.java
+++ Coding_Patterns/Two-pointers/basic-two-pointers/src/com/manhpd/LongPressedName.java
@@ -36,27 +36,58 @@ package com.manhpd;
 public class LongPressedName {
 
     public static void main(String[] args) {
-        String name = "leelee";
-        String typed = "lleeelee";
+//        String name = "leelee";
+//        String typed = "lleeelee";
+
+        String name = "saeed";
+        String typed = "ssaaedd";
 
         boolean res = isLongPressedName(name, typed);
         System.out.println(res);
     }
 
-    public static boolean isLongPressedName(String name, String typed) {
-        for (int iName = 0, iTyped = 0; iName < name.length() || iTyped < typed.length();) {
+    public static boolean isLongPressedName0(String name, String typed) {
+        int iName = 0;
+        int iTyped = 0;
+
+        for (; iName < name.length() && iTyped < typed.length();) {
             char nameChar = name.charAt(iName);
             char typedChar = typed.charAt(iTyped);
+            char nextNameChar = iName < name.length() - 1 ? name.charAt(iName + 1) : 0;
 
-            if (nameChar == typedChar) {
+            if (nameChar == typedChar && nextNameChar != nameChar) {
                 ++iTyped;
             } else {
                 ++iName;
             }
         }
 
+        return (iTyped == typed.length()) || (iName == name.length() - 1);
+    }
 
-        return false;
+    public static boolean isLongPressedName(String name, String typed) {
+        NumChar[] numNameChars = new NumChar[1000];
+        NumChar[] numTypedChars = new NumChar[1000];
+
+        for (int iName = 0, count = 0; iName < name.length(); ++iName) {
+            char nameChar = name.charAt(iName);
+            if (nameChar != numNameChars[count].c) {
+                ++count;
+            }
+
+            numNameChars[count].c = nameChar;
+            ++numNameChars[count].num;
+        }
+
+        for (int iTyped = 0; iTyped < name.length(); ++iTyped) {
+
+        }
+
+    }
+
+    class NumChar {
+        char c = 0;
+        int num = 0;
     }
 
 }
