76dc5431650fa93adf7de855be63f0ea552091c0
Phan Manh <ducmanhphan93@gmail.com>
2020-11-05 20:33:34 +0700

    update files

--- Algorithms/Pattern-Matching-Algorithm/rete-algorithm/src/main/java/com/mp/objects/ConditionInfo.java
+++ Algorithms/Pattern-Matching-Algorithm/rete-algorithm/src/main/java/com/mp/objects/ConditionInfo.java
@@ -3,11 +3,9 @@ package com.mp.objects;
 import com.mp.constants.WMEFieldType;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
-@NoArgsConstructor
 public class ConditionInfo {
 
     private WMEFieldType fieldArg2;
