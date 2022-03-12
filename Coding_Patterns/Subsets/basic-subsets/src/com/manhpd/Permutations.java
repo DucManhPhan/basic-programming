937693836c66acee830b1cb27a0e208f3d5071c4
Phan Manh <ducmanhphan93@gmail.com>
2022-03-12 11:01:09 +0700

    update files

--- Coding_Patterns/Subsets/basic-subsets/src/com/manhpd/Permutations.java
+++ Coding_Patterns/Subsets/basic-subsets/src/com/manhpd/Permutations.java
@@ -27,10 +27,17 @@ import java.util.Queue;
 public class Permutations {
 
     public static void main(String[] args) {
-        List<List<Integer>> result = Permutations.findPermutations(new int[] { 1, 3, 5 });
+//        List<List<Integer>> result = Permutations.findPermutations(new int[] { 1, 3, 5 });
+        List<List<Integer>> result = Permutations.findPermutationsWithRecursive(new int[] { 1, 3, 5 });
         System.out.print("Here are all the permutations: " + result);
     }
 
+    /**
+     * Using BFS algorithm
+     *
+     * @param nums
+     * @return
+     */
     public static List<List<Integer>> findPermutations(int[] nums) {
         List<List<Integer>> result = new ArrayList<>();
         Queue<List<Integer>> permutations = new LinkedList<>();
@@ -56,4 +63,33 @@ public class Permutations {
 
         return result;
     }
+
+    /**
+     * Using recursive algorithm
+     *
+     * @param nums
+     * @return
+     */
+    public static List<List<Integer>> findPermutationsWithRecursive(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        findPermutationsWithRecursive(nums, 0, new ArrayList<>(), result);
+
+        return result;
+    }
+
+    private static void findPermutationsWithRecursive(int[] nums, int index, List<Integer> currentPermutation,
+                                                     List<List<Integer>> result) {
+        if (currentPermutation.size() == nums.length) {
+            result.add(currentPermutation);
+            return;
+        }
+
+        for (int i = 0; i <= currentPermutation.size(); ++i) {
+            List<Integer> newPermutation = new ArrayList<>(currentPermutation);
+            newPermutation.add(i, nums[index]);
+
+            findPermutationsWithRecursive(nums, index + 1, newPermutation, result);
+        }
+    }
+
 }
