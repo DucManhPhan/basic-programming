d34c257c441a92a1dbffd7d9a6df7a060633240c
Phan Manh <ducmanhphan93@gmail.com>
2022-04-18 07:19:22 +0700

    update files

--- Algorithms/Dynamic Programming/basic-dp/src/com/manhpd/patternFibonacciNumber/MinimumJumpsReachEnd.java
+++ Algorithms/Dynamic Programming/basic-dp/src/com/manhpd/patternFibonacciNumber/MinimumJumpsReachEnd.java
@@ -1,7 +1,9 @@
 package com.manhpd.patternFibonacciNumber;
 
 /**
- * Given an array of positive numbers, where each element represents the max number of jumps that can be made forward from that element, write a program to find the minimum number of jumps needed to reach the end of the array (starting from the first element). If an element is 0, then we cannot move through that element.
+ * Given an array of positive numbers, where each element represents the max number of jumps that can be made forward from that element,
+ * write a program to find the minimum number of jumps needed to reach the end of the array (starting from the first element).
+ * If an element is 0, then we cannot move through that element.
  *
  * Example 1:
  *
