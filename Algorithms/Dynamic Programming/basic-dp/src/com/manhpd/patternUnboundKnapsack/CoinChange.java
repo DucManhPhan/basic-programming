c4fa5216a50cc7b519cb477f1a4f4beea3286914
Phan Manh <ducmanhphan93@gmail.com>
2022-10-17 09:12:09 +0700

    update files

--- Algorithms/Dynamic Programming/basic-dp/src/com/manhpd/patternUnboundKnapsack/CoinChange.java
+++ Algorithms/Dynamic Programming/basic-dp/src/com/manhpd/patternUnboundKnapsack/CoinChange.java
@@ -18,15 +18,57 @@ package com.manhpd.patternUnboundKnapsack;
 public class CoinChange {
 
     public static void main(String[] args) {
-//        int[] denominations = {1, 2, 5};
-//        int total = 5;
+        int[] denominations = {1, 2, 3};
+        int total = 5;
 
-        int[] denominations = {3};
-        int total = 2;
+//        int[] denominations = {3};
+//        int total = 2;
         CoinChange solution = new CoinChange();
-        System.out.println(solution.countChange(denominations, total));
+//        System.out.println(solution.countChange(denominations, total));
+        System.out.println(solution.countChangeRecursion(denominations, total));
     }
 
+    /**
+     * Using Recursion algorithm
+     *
+     * @param coins
+     * @param amount
+     * @return
+     */
+    public int countChangeRecursion(int[] coins, int amount) {
+        return this.countChange(coins, amount, 0);
+    }
+
+    private int countChange(int[] coins, int amount, int idx) {
+        // base case
+        if (amount == 0) {
+            return 1;
+        }
+
+        if (amount < coins[idx] || idx >= coins.length) {
+            return 0;
+        }
+
+        // choose the coin at idx index
+        int num1 = countChange(coins, amount - coins[idx], idx);
+
+        // not choose the coin at idx
+        int num2 = 0;
+        if (idx < coins.length - 1) {
+            num2 = countChange(coins, amount, idx + 1);
+        }
+
+        System.out.println("(" + amount + ", " + idx + ") = " + (num1 + num2));
+        return num1 + num2;
+    }
+
+    /**
+     * Using Bottom-up DP
+     *
+     * @param coins
+     * @param amount
+     * @return
+     */
     public int countChange(int[] coins, int amount) {
         if (coins.length == 0) {
             return amount == 0 ? 1 : 0;
