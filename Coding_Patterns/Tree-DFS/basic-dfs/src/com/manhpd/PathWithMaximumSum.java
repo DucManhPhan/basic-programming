81705e547eadb6d8a8e71e898b33bb6d5f2a0d0d
Phan Manh <ducmanhphan93@gmail.com>
2023-01-16 13:47:20 +0700

    update files

--- Coding_Patterns/Tree-DFS/basic-dfs/src/com/manhpd/PathWithMaximumSum.java
+++ Coding_Patterns/Tree-DFS/basic-dfs/src/com/manhpd/PathWithMaximumSum.java
@@ -1,5 +1,9 @@
 package com.manhpd;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * Find the path with the maximum sum in a given binary tree. Write a function that returns the maximum sum.
  * A path can be defined as a sequence of nodes between any two nodes and doesn’t necessarily pass through the root.
@@ -46,14 +50,24 @@ public class PathWithMaximumSum {
     }
 
     public static void main(String[] args) {
-        TreeNode root = buildExample1();
-        System.out.println("Maximum Path Sum: " + PathWithMaximumSum.findMaximumPathSum(root));
+//        TreeNode root = buildExample1();
+//        System.out.println("Maximum Path Sum: " + PathWithMaximumSum.findMaximumPathSum(root));
 
 //        TreeNode root = buildExample2();
 //        System.out.println("Maximum Path Sum: " + PathWithMaximumSum.findMaximumPathSum(root));
 
 //        Tree Node root = buildExample3();
 //        System.out.println("Maximum Path Sum: " + PathWithMaximumSum.findMaximumPathSum(root));
+
+        List<String> commands = new ArrayList<>() {{
+            add("open firefox");
+            add("open curl");
+            add("open terminal");
+            add("close 2");
+            add("open ps");
+        }};
+
+        getOpenApplications(commands);
     }
 
     private static TreeNode buildExample1() {
@@ -89,6 +103,51 @@ public class PathWithMaximumSum {
         root.right.right.left = new TreeNode(9);
 
         return root;
+    }
+
+    public static List<String> getOpenApplications(List<String> commands) {
+        // Write your code here
+        if (commands == null || commands.size() == 0) {
+            return new LinkedList<>();
+        }
+
+        List<String> openApplications = new LinkedList<>();
+        for (String command : commands) {
+            System.out.println(command);
+
+            String name = extractApplicationName(command);
+
+            if (command.contains("open")) {
+                openApplications.add(name);
+            } else if (command.contains("close")) {
+                int k = Integer.valueOf(name);
+                System.out.println("Close number of app: " + k);
+
+                if (k >= openApplications.size()) {
+                    openApplications.clear();
+                    continue;
+                }
+
+                int length = openApplications.size();
+                for (int i = length - 1;; --i) {
+                    if (length - i == k + 1) {
+                        break;
+                    }
+
+                    openApplications.remove(i);
+                }
+            } else if (command.contains("clear")) {
+                openApplications.clear();
+            }
+        }
+
+        System.out.println(openApplications.toArray());
+        return openApplications;
+    }
+
+    private static String extractApplicationName(String command) {
+        int idxOfSpace = command.indexOf(' ');
+        return command.substring(idxOfSpace + 1, command.length());
     }
 
 }
