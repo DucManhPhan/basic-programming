dfc4feed6993fe85af4f8d30ab7a5d9c4f4f8944
Phan Manh <ducmanhphan93@gmail.com>
2023-01-16 17:10:24 +0700

    update files

--- Coding_Patterns/Cyclic-sort/src/com/manhpd/FindMissingNumber.java
+++ Coding_Patterns/Cyclic-sort/src/com/manhpd/FindMissingNumber.java
@@ -1,5 +1,11 @@
 package com.manhpd;
 
+import java.util.HashSet;
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 /**
  * We are given an array containing n distinct numbers taken from the range 0 to n.
  * Since the array has only n numbers out of the total n+1 numbers, find the missing number.
@@ -15,6 +21,12 @@ package com.manhpd;
  */
 public class FindMissingNumber {
 
+    /**
+     * 1st way: Using Cyclic Sort
+     *
+     * @param nums
+     * @return
+     */
     public static int findMissingNumber(int[] nums) {
         int startIdx = 0;
 
@@ -43,9 +55,60 @@ public class FindMissingNumber {
         nums[y] = tmp;
     }
 
+    /**
+     * 2nd way: Using brute force
+     *
+     * @param nums
+     * @return
+     */
+    public static int findMissingNumberV1(int[] nums) {
+        int size = nums.length;
+
+        // bubble sort
+        for (int i = 0; i < size; ++i) {
+            for (int j = 0; j < size - i - 1; ++j) {
+                if (nums[j] > nums[j + 1]) {
+                    swap(nums, j, j + 1);
+                }
+            }
+        }
+
+        // find the first item's value and its index is different
+        for (int i = 0; i < size; ++i) {
+            if (i != nums[i]) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
+    /**
+     * 3rd way: Using HashSet to contain a range from [0, n]
+     *
+     * @param nums
+     * @return
+     */
+    public static int findMissingNumberV2(int[] nums) {
+        int size = nums.length;
+
+        Set<Integer> fullNumbers = new HashSet<>();
+        IntStream.range(0, size)
+                 .forEach(i -> fullNumbers.add(nums[i]));
+
+        for (int i = 0; i < size; ++i) {
+            if (!fullNumbers.contains(i)) {
+                return i;
+            }
+        }
+
+        return size;
+    }
+
     public static void main(String[] args) {
         int[] nums = {4, 0, 3, 1};
-        int missing = findMissingNumber(nums);
+//        int missing = findMissingNumber(nums);
+        int missing = findMissingNumberV1(nums);
 
         System.out.println("Missing number: " + missing);
     }
