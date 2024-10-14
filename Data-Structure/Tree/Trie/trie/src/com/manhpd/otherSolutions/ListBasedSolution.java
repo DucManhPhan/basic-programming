bcbefdafe0d13a8e55bb5b30ff05dcf170ddf973
Phan Manh <ducmanhphan93@gmail.com>
2024-10-14 11:45:39 +0700

    update files

---
+++ Data-Structure/Tree/Trie/trie/src/com/manhpd/otherSolutions/ListBasedSolution.java
@@ -0,0 +1,4 @@
+package com.manhpd.otherSolutions;
+
+public class ListBasedSolution {
+}
---
+++ Data-Structure/Tree/Trie/trie/src/com/manhpd/otherSolutions/SortedListBasedSolution.java
@@ -0,0 +1,29 @@
+package com.manhpd.otherSolutions;
+
+import java.util.Collections;
+import java.util.List;
+
+public class SortedListBasedSolution {
+
+    public static void main(String[] args) {
+        String[] strings = {
+                "apple", "ant", "bin", "back", "ball", "big", "brick", "bribe", "broke", "bride", "brag", "bridge", "bright", "brisket", "yellow", "wood"
+        };
+
+        List<String> results = sortedListBasedSolution(strings);
+        System.out.println("Results: " + results);
+    }
+
+    /**
+     * Sort the input that has array list of strings first.
+     * Using binary search to find the correct incremental prefix
+     *
+     * @param input
+     * @return
+     */
+    private static List<String> sortedListBasedSolution(String[] input) {
+
+
+        return Collections.emptyList();
+    }
+}
