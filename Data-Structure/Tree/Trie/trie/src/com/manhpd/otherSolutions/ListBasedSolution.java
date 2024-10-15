b1764000413b05e72b59a9807e704e4619eab191
Phan Manh <ducmanhphan93@gmail.com>
2024-10-15 11:42:47 +0700

    update files

--- Data-Structure/Tree/Trie/trie/src/com/manhpd/otherSolutions/ListBasedSolution.java
+++ Data-Structure/Tree/Trie/trie/src/com/manhpd/otherSolutions/ListBasedSolution.java
@@ -1,4 +1,29 @@
 package com.manhpd.otherSolutions;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ListBasedSolution {
+
+    public static void main(String[] args) {
+        String[] strings = {
+                "apple", "ant", "bin", "back", "ball", "big", "brick", "bribe", "broke", "bride", "brag", "bridge", "bright", "brisket", "yellow", "wood",
+        };
+        String prefix = "bri";
+
+        List<String> res = listBasedSolution(strings, prefix);
+        System.out.println(res.toString());
+    }
+
+    private static List<String> listBasedSolution(String[] strings, String prefix) {
+        List<String> res = new ArrayList<>();
+
+        for (String str : strings) {
+            if (str.startsWith(prefix)) {
+                res.add(str);
+            }
+        }
+
+        return res;
+    }
 }
--- Data-Structure/Tree/Trie/trie/src/com/manhpd/otherSolutions/SortedListBasedSolution.java
+++ Data-Structure/Tree/Trie/trie/src/com/manhpd/otherSolutions/SortedListBasedSolution.java
@@ -1,29 +1,93 @@
 package com.manhpd.otherSolutions;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.Arrays;
 
 public class SortedListBasedSolution {
 
     public static void main(String[] args) {
         String[] strings = {
-                "apple", "ant", "bin", "back", "ball", "big", "brick", "bribe", "broke", "bride", "brag", "bridge", "bright", "brisket", "yellow", "wood"
+                 "bright", "brisket", "yellow", "wood", "apple", "ant", "bin", "back", "ball", "big", "brick", "bribe", "broke", "bride", "brag", "bridge",
         };
 
-        List<String> results = sortedListBasedSolution(strings);
-        System.out.println("Results: " + results);
+        String prefix = "bri";
+        String[] results = sortedListBasedSolution(strings, prefix);
+        System.out.println("Results: " + Arrays.toString(results));
     }
 
     /**
      * Sort the input that has array list of strings first.
      * Using binary search to find the correct incremental prefix
      *
-     * @param input
+     * @param strings
      * @return
      */
-    private static List<String> sortedListBasedSolution(String[] input) {
+    private static String[] sortedListBasedSolution(String[] strings, String prefix) {
+        Arrays.sort(strings);
+        System.out.println(Arrays.toString(strings));
 
+        int lengthPrefix = prefix.length();
+        for (int i = 1; i < lengthPrefix + 1; ++i) {
+            String prefixToSearch = prefix.substring(0, i);
+            System.out.println("prefixToSearch: " + prefixToSearch);
 
-        return Collections.emptyList();
+            int startIdx = lowerBound(strings, prefixToSearch);
+            int endIdx = upperBound(strings, prefixToSearch);
+            System.out.println("startIdx = " + startIdx + ", endIdx = " + endIdx);
+
+            strings = Arrays.copyOfRange(strings, startIdx, endIdx + 1, String[].class);
+            System.out.println("current strings: " + Arrays.toString(strings));
+        }
+
+        return strings;
+    }
+
+    private static int lowerBound(String[] strings, String prefix) {
+        if (strings == null || strings.length == 0) {
+            return -1;
+        }
+
+        int left = 0;
+        int right = strings.length;
+
+        while (left + 1 < right) {
+            int mid = (right + left) / 2;
+
+            if (strings[mid].startsWith(prefix)) {
+                right = mid;
+            } else {
+                left = mid;
+            }
+        }
+
+        if (strings[right].startsWith(prefix)) {
+            return right;
+        }
+
+        return -1;
+    }
+
+    private static int upperBound(String[] strings, String prefix) {
+        if (strings == null || strings.length == 0) {
+            return -1;
+        }
+
+        int left = 0;
+        int right = strings.length;
+
+        while (left + 1 < right) {
+            int mid = (right + left) / 2;
+
+            if (strings[mid].startsWith(prefix)) {
+                left = mid;
+            } else {
+                right = mid;
+            }
+        }
+
+        if (strings[left].startsWith(prefix)) {
+            return left;
+        }
+
+        return -1;
     }
 }
