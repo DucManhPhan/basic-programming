8a8cbfdeab31202a156fca305e3681d3ef74c6ea
Phan Manh <ducmanhphan93@gmail.com>
2024-11-01 22:47:03 +0700

    update files

--- Coding_Patterns/Sliding-window/src/com/manhpd/java/WordsConcatention.java
+++ Coding_Patterns/Sliding-window/src/com/manhpd/java/WordsConcatention.java
@@ -4,8 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Ref: https://www.educative.io/courses/grokking-the-coding-interview/Y5YDWzqPn7O
- *
  * Given a string and a list of words, find all the starting indices of substrings in the given string
  * that are a concatenation of all the given words exactly once without any overlapping of words.
  * It is given that all words are of the same length.
