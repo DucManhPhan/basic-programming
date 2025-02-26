22981a71a09f9ba5640ce0dec55335750da6af32
unknown <ducmanhphan93@gmail.com>
2025-02-26 17:01:19 +0700

    update files

--- Algorithms/Dynamic Programming/basic-dp/src/com/manhpd/patternKnapsack01/EqualSubsetSumPartition.java
+++ Algorithms/Dynamic Programming/basic-dp/src/com/manhpd/patternKnapsack01/EqualSubsetSumPartition.java
@@ -21,13 +21,32 @@ import java.util.Arrays;
 public class EqualSubsetSumPartition {
 
     public static void main(String[] args) {
+        // Example 1
         int[] nums = {1, 2, 3, 4};
-        boolean canPartition = canPartition(nums);
+        boolean res = true;
+
+        // Example 2
+//        int[] nums = {1, 1, 3, 4, 7};
+//        boolean res = true;
+
+        // Example 3
+//        int[] nums = {2, 3, 4, 6};
+//        boolean res = false;
+
+//        boolean canPartition = canPartitionV1(nums);
+//        boolean canPartition = canPartitionV2(nums);
+        boolean canPartition = canPartitionV3(nums);
 
         System.out.println(canPartition);
     }
 
-    public static boolean canPartition(int[] num) {
+    /**
+     * Using recursion version to solve it.
+     *
+     * @param num
+     * @return
+     */
+    public static boolean canPartitionV1(int[] num) {
         if (num == null || num.length == 0) {
             return false;
         }
@@ -37,11 +56,11 @@ public class EqualSubsetSumPartition {
             return false;
         }
 
-        boolean canPartition = canPartition(num, 0, sum/2);
+        boolean canPartition = canPartitionV1(num, 0, sum/2);
         return canPartition;
     }
 
-    public static boolean canPartition(int[] num, int currentIndex, int target) {
+    public static boolean canPartitionV1(int[] num, int currentIndex, int target) {
         if (currentIndex >= num.length || target < 0) {
             return false;
         }
@@ -53,11 +72,103 @@ public class EqualSubsetSumPartition {
         boolean isAdded = false;
         boolean isSkipped = false;
         if (num[currentIndex] <= target) {
-            isAdded = canPartition(num, currentIndex + 1, target - num[currentIndex]);
+            isAdded = canPartitionV1(num, currentIndex + 1, target - num[currentIndex]);
         }
 
-        isSkipped = canPartition(num, currentIndex + 1, target);
+        isSkipped = canPartitionV1(num, currentIndex + 1, target);
         return isAdded || isSkipped;
     }
 
+    /**
+     * Use Dynamic Programming with Memoization
+     *
+     * @param num
+     * @return
+     */
+    public static boolean canPartitionV2(int[] num) {
+        if (num == null || num.length == 0) {
+            return false;
+        }
+
+        int sum = Arrays.stream(num).sum();
+        if ((sum & 1) != 0) {
+            return false;
+        }
+
+        Boolean[][] dp = new Boolean[num.length][(sum/2) + 1];
+
+        boolean canPartition = canPartitionV2(num, dp, 0, sum/2);
+        return canPartition;
+    }
+
+    private static boolean canPartitionV2(int[] num, Boolean[][] dp, int idx, int halfSum) {
+        if (idx >= num.length || halfSum < 0) {
+            return false;
+        }
+
+        if (halfSum == 0) {
+            dp[idx][halfSum] = true;
+            return true;
+        }
+
+        if (dp[idx][halfSum] != null) {
+            return dp[idx][halfSum];
+        }
+
+        boolean isAdded = false;
+        boolean isSkipped = false;
+
+        if (num[idx] <= halfSum) {
+            isAdded = canPartitionV2(num, dp, idx + 1, halfSum - num[idx]);
+        }
+
+        isSkipped = canPartitionV2(num, dp, idx + 1, halfSum);
+        dp[idx][halfSum] = isAdded || isSkipped;
+
+        return dp[idx][halfSum];
+    }
+
+    /**
+     * Using Dynamic Programming with Tabulation
+     *
+     * @param num
+     * @return
+     */
+    public static boolean canPartitionV3(int[] num) {
+        if (num == null || num.length == 0) {
+            return false;
+        }
+
+        int sum = Arrays.stream(num).sum();
+        if ((sum & 1) != 0) {
+            return false;
+        }
+
+        int halfSum = sum/2;
+        boolean[][] dp = new boolean[num.length][halfSum + 1];
+
+        // initialize boolean array
+        for (int i = 0; i < num.length; ++i) {
+            dp[i][0] = false;
+        }
+
+        for (int i = 1; i <= halfSum; ++i) {
+            dp[0][i] = false;
+        }
+
+        boolean res = false;
+        for (int i = 1; i < num.length; ++i) {
+            for (int j = 1; j <= halfSum; ++j) {
+                res = false;
+
+                if (num[i] <= j) {
+                    res = dp[i - 1][j - num[i]];
+                }
+
+                dp[i][halfSum] = res | dp[i - 1][j];
+            }
+        }
+
+        return dp[num.length - 1][halfSum];
+    }
 }
